f2b606d00f78ba6b90a6f71244ef41137fc21835
RushikeshNagawade <85493813+RushikeshNagawade@users.noreply.github.com>
2021-06-12 12:50:13 +0530

    comt

--- src/com/cg/demo/lambda/LambdaDemo2.java
+++ src/com/cg/demo/lambda/LambdaDemo2.java
@@ -1,5 +1,18 @@
 package com.cg.demo.lambda;
 
 public class LambdaDemo2 {
+	/*float result;
+	public static void main(String args[]) {
+		result = 5/2;
+		System.out.println(result);*/
+		public static void main(String[] args) {
+			int $ = 8;
+			System.out.println($);
+		/** when
+		 * 
+		 */
+		}
+		
+		
+	}
 
-}
